59938c540fff480d44a125c318a62208479ff8b6
othmantaaban <othman.taabn03@gmail.com>
2023-10-19 19:20:37 +0100

    some modification in components

--- src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
+++ src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -43,4 +43,15 @@ export class HorizontalBarChartComponent implements OnInit {
     console.log('bar data', this.barChartData)
   }
 
+  NotOnlyZero(array) {
+    if(Array.isArray(array)) {
+      for (let i = 0; i < array.length; i++) {
+        if(array[i] > 0) {
+          return true
+        }      
+      }
+    }
+    return false;
+  }
+
 }
